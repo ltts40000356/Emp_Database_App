1543f20dcb8bdc65fe1e8881c26699232076e99e
Arunava Bhattacharya <arunavabhattacharya89@gmail.com>
2021-06-03 19:41:23 +0530

    Initial commit

---
+++ venv/main.py
@@ -0,0 +1,3 @@
+# This is a simple App that does basic CRUD operations on Data stored in a file
+# This is the main python script that invokes functions in other files
+# Coded By: Arunava Bhattacharya
